3f8b5ea51de0df8991b125a05f4803053a0ee321
John Biddiscombe <biddisco@cscs.ch>
2012-04-19 09:16:45 +0200

    Disable power save/cleep mode when martycam is running

--- main.cpp
+++ main.cpp
@@ -5,16 +5,33 @@
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 #ifdef WIN32
- AllocConsole();
- freopen("conin$", "r", stdin);
- freopen("conout$", "w", stdout);
- freopen("conout$", "w", stderr);
+  AllocConsole();
+  freopen("conin$", "r", stdin);
+  freopen("conout$", "w", stdout);
+  freopen("conout$", "w", stderr);
+
+  //
+  // Prevent machine going to sleep whilst MartyCam is running
+  //
+  if (SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED) == NULL)
+  {
+     // try XP variant as well just to make sure 
+     SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED);
+  }  
 #endif
+
   QApplication app(nCmdShow, &lpCmdLine);
   QCoreApplication::setOrganizationName("MartyCam");
   QCoreApplication::setOrganizationDomain("MartyCam");
   QCoreApplication::setApplicationName("MartyCam");
   MartyCam *tracker = new MartyCam();
   tracker->show();
-  return app.exec();
+  app.exec();
+  //
+  // set power state back to normal
+  //
+#ifdef WIN32
+  SetThreadExecutionState(ES_CONTINUOUS);  
+#endif
+  return 0;
 }
